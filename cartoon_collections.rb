cedfcfe68ea357c1634d6cf0f00a9e3212478fdf
Jed Peek <jedpeek@yahoo.com>
2018-06-04 23:44:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,9 +25,7 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if arr.each{|cheese| cheese == cheese_types.each{|x| return x}}
-  return cheese
-else
-  return nil
-end
+i = 0
+while i < arr.size
+  if arr[i] == cheese_types.each{|x| puts x}
 end
